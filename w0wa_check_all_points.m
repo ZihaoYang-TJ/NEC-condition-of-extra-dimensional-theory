f9fc1b90365c6233278621b72a41fb5ab2fd6931
Zihao Yang <164285235+ZihaoYang-TJ@users.noreply.github.com>
2024-06-06 20:53:11 +0800

    Add files via upload

--- w0wa_check_all_points.m
+++ w0wa_check_all_points.m
@@ -1,3 +1,5 @@
+function w0wa_check_all_points(omega0,zeta0,k)
+
 w0scope=-1:0.01:-0.3;
 wascope=-1:0.01:1;
 warning off
@@ -8,15 +10,15 @@ for i=1:71
 w0=w0scope(i); wa=wascope(j);
 
 %w_phi=w0+wa*z/(1+z);
-[N,O] = ode45(@(t,y) -3*(w0+wa*(1-exp(t)))*y*y*(1-y),[0,log(0.1)] , 0.7);
+[N,O] = ode45(@(t,y) -3*(w0+wa*(1-exp(t)))*y*y*(1-y),[0,log(0.1)] , omega0);
 w=(w0+wa*(1-exp(N))).*O;
-F=(9/2*(1+w)).^0.5;
+F=(6*k/(2+k)*(1+w)).^0.5;
 all_greater_than_minus_one = all(w(:) > -1);
-[N_r,O_r] = ode45(@(t,y) -3*(w0+wa*(1-exp(t)))*y*y*(1-y),[0,log(2)] , 0.7);
+[N_r,O_r] = ode45(@(t,y) -3*(w0+wa*(1-exp(t)))*y*y*(1-y),[0,log(2)] , omega0);
 w_r=(w0+wa*(1-exp(N_r))).*O_r;
 all_greater_than_minus_one_r = all(w_r(:) > -1);
 %plot(exp(N),w)
-[t,y] = ode45(@(t,y) myode(t,y,N,w), [0,log(0.1)], 0.00614);
+[t,y] = ode45(@(t,y) myode(t,y,N,w), [0,log(0.1)], zeta0);
         if y(end)>-F(end)
             if all_greater_than_minus_one
                 if all_greater_than_minus_one_r
@@ -28,9 +30,13 @@ all_greater_than_minus_one_r = all(w_r(:) > -1);
     end
 end
 axis([-1,-0.3,-1,1])
-xlabel('$w_0$','interpreter','latex');ylabel('$w_a$','interpreter','latex')
-
-
+xlabel('$w_0$','FontSize', 22,'interpreter','latex');ylabel('$w_a$','FontSize', 22,'interpreter','latex');
+x=[635 498 413 336 308 279 210 158 113 118 163 216 289 420];
+y=[449 340 270 206 147 126 109 92 107 140 208 274 350 486];
+w0=(x-630)*(1/(630-283));
+wa=(200-y)*(1/(200-104));
+plot(w0,wa,'r--')
+% alpha(1)
 %[N,O] = ode45(@(t,y) -3*(w0+wa*(1-exp(t)))*y*y*(1-y),[0,log(2)] , 0.7);
 %w=(w0+wa*(1-exp(N))).*O;
 %plot(exp(N),w)
@@ -38,5 +44,7 @@ xlabel('$w_0$','interpreter','latex');ylabel('$w_a$','interpreter','latex')
 %F=(9/2*(1+w)).^0.5;
 function dydt = myode(t,y,N,w)
 w = interp1(N,w,t); % Interpolate the data set (ft,f) at time t
-dydt = y^2+3*(w-1)/2*y-9/4*(1+3*w); % Evaluate ODE at time t
+dydt = y^2+3*(w-1)/2*y-3*k/(k+2)*(1+3*w); % Evaluate ODE at time t
+end
+
 end
